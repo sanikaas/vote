3a8cc3d552074ef1d529990780ee48c2d3bf6e87
sanikaas <sanikaas13@gamil.com>
2025-10-05 17:42:34 +0530

    This is swap program

---
+++ swap.py
@@ -0,0 +1,16 @@
+# swap_numbers.py
+
+# Get input from user
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+
+print("\nBefore swapping:")
+print("First number (a):", a)
+print("Second number (b):", b)
+
+# Swap the values
+a, b = b, a
+
+print("\nAfter swapping:")
+print("First number (a):", a)
+print("Second number (b):", b)
